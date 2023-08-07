b4f1ce216b97df45571a0a69093fb0e8d1935bb3
JoaoVitorLira <joao.liraves07@gmail.com>
2023-08-07 10:37:04 -0300

    Conclusão da atividade

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ function vericacaodeacesso(){
 
     if (cartao >= 1000 && cartao <= 1999){
         resposta = "Possuí um acesso geral a todas áreas restritas!"
+        document.getElementById('resultado').style.color = 'green'
+
     } else if (cartao >= 2000 && cartao <= 2999){
         resposta = "Possuí um acesso limitado a algumas áreas restritas!"
+        document.getElementById('resultado').style.color = 'chocolate'
     } else {
         resposta = "Não possuí um acesso a áreas restritas!"
-        
+        document.getElementById('resultado').style.color = 'red'   
     }
 
-    document.getElementById("resultado").textContent = resposta;
+    document.getElementById('resultado').textContent = resposta;
 }
